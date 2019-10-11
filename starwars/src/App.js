cdd7b5d1db06d8a99f6c2f8d7d38abcba146b31e
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-10-11 14:48:17 -0500

    mvp completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import styled from 'styled-components';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 import CharacterCard from './components/CharacterCard';
 
-const MainContent = styled.section`
+const Main = styled.section`
 width: 100%;
 margin: 0 auto;
 height: 100vh;
@@ -19,6 +19,7 @@ border: 1px solid black;
 border-radius: 10px;
 font-size: 1rem;
 font-weight: 600;
+cursor: pointer;
 opacity: 0.8;
 `;
 
@@ -30,12 +31,12 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [data, setData] = useState();
+  const [data, setData] = useState([{}]);
   const [page, setPage] = useState();
 
   useEffect(() => {
     axios
-      .get(`https://swapi.co/api/people/?page=${page}`)
+      .get(`https://swapi.co/api/people/?page${page}`)
 
       .then(response => {
         setData(response.data.results);
@@ -51,7 +52,7 @@ const App = () => {
   return (
     <>
       <Nav />
-      <MainContent className="App">
+      <Main className="App">
 
         <Button className="next" onClick={() => setPage(page + 1)}>Next</Button>
 
@@ -61,9 +62,14 @@ const App = () => {
           return <CharacterCard name={character.name} eye_color={character.eye_color} gender={character.gender} birth_year={character.birth_year} url={character.url} />
         }
         )}
-      </MainContent>
+
+        <Button className="next" onClick={() => setPage(page + 1)}>Next</Button>
+
+        <Button className="previous" onClick={() => setPage(page - 1)}>Prev</Button>
+
+      </Main>
     </>
   );
 }
 
-export default App;
+export default App
